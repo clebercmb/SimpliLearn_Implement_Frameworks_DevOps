4ab76594433bd709f8467a09917ed25444154faa
Cleber Miranda Barbosa <cleber@gmail.com>
2021-07-10 13:43:13 -0400

    day 05

---
+++ day_03_spring_jdbc_demo/src/main/java/com/example/Day03SpringJdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day03SpringJdbcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day03SpringJdbcDemoApplication.class, args);
+    }
+
+}
---
+++ day_05/src/test/java/com/example/day_05/Day05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
